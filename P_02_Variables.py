d78095c7f2520762a585bf5a9e996ec961fbb070
finallydevil <mohammedn4063@masseyhigh.school.nz>
2025-07-22 13:02:27 +1200

    variable basics

---
+++ P_02_Variables.py
@@ -0,0 +1,13 @@
+# code to learn about variables...
+
+# variables can be a string (note the speech marks)
+greeting = "Hi"
+
+# Variables cn be a number
+fav_num = 7
+num_string = "5"
+
+# Constants are uppercase
+PI = 3.14
+
+a_01_my_num = 7
